ab263ac9522ccbec3ad94002d904e23ce9dbe71c
tuhindewan <tuhinsshadow@gmail.com>
2017-04-03 09:42:34 -0700

    uploada general image

--- index.php
+++ index.php
@@ -9,7 +9,35 @@ $db = new Database();
 
 ?>
 	<section class="mainoption">
+
 		 <div class="myform">
+
+		 <?php
+
+if($_SERVER['REQUEST_METHOD']=="POST" && isset($_POST['submit'])){
+	
+	$permited = array('jpg', 'jpeg', 'png', 'gif');
+
+	 $filename = $_FILES['image']['name'];
+	 $fileSize = $_FILES['image']['size'];
+	 $filetemp = $_FILES['image']['tmp_name'];
+
+	 $folder = "uploads/";
+	 move_uploaded_file( $filetemp,$folder.$filename);
+
+	 $query = "INSERT INTO tbl_image (imageName) VALUES ('$filename')";
+
+	 $inserted_rows = $db->insert($query);
+	 if($inserted_rows){
+			echo "<span class='success'>Image Uploaded Successully.</span>";
+	 }else{
+			echo "<span class='error'>Image Uploaded Successully.</span>";
+	 }
+
+}
+
+
+?>
 		  <form action="" method="post" enctype="multipart/form-data">
 		   <table>
 		    <tr>
@@ -23,6 +51,7 @@ $db = new Database();
 		   </table>
 		  </form>
 		 </div>
+
 	</section>
 
 <?php include "inc/footer.php";?>
